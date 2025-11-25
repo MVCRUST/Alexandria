ab7d60e6de144575f2738bcf6a896ffd11273ae9
Max Crust <max.crust7@gmail.com>
2025-11-25 16:31:01 +0000

    Update index.js
    Updated the javascript

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Select elements from DOM
 const bookForm = document.querySelector("#inputSection form");
-const bookList = document.querySelector("#searchResults ul");
+const bookList = document.querySelector("#searchResults tbody");
 
 // Add an event listener to submit button that fetches book data using API
 bookForm.addEventListener("submit", extractBook);
@@ -20,6 +20,7 @@ async function fetchBookData(book) {
     );
     if (respData.ok) {
       const bookData = await respData.json();
+      bookList.innerHTML = '<table></table>'
       addBook(bookData);
     } else {
       throw "Something went wrong with your search.";
@@ -30,8 +31,37 @@ async function fetchBookData(book) {
 }
 
 // Define function that creates an item in the results list with the name of the first book found by the API.
+// For future reference if we want to start placing the list into a table instead of a list
+// function addBook(book) {
+//   for (let i = 1; i < 15; i++) {
+//     const li = document.createElement("li");
+//     const liAuthor = document.createElement("li");
+//     li.textContent = book.docs[i].title;
+//     bookList.appendChild(li);
+//     liAuthor.textContent = book.docs[i].author_name;
+//     bookList.appendChild(liAuthor);
+//   }
+// }
+
 function addBook(book) {
-  const li = document.createElement("li");
-  li.textContent = book.docs[0].title;
-  bookList.appendChild(li);
+  for (let i = 1; i < 15; i++) {
+    const row = document.createElement("tr");
+    const cell = document.createElement("td");
+    cell.textContent = book.docs[i].title;
+    const cell2 = document.createElement("td");
+    cell2.textContent = book.docs[i].author_name;
+    const cell3 = document.createElement("td");
+    cell3.textContent = book.docs[i].language[0];
+    const cell4 = document.createElement("td");
+    cell4.textContent = book.docs[i].edition_count;
+    const cell5 = document.createElement("td");
+    cell5.textContent = book.docs[i].has_fulltext;
+    //+ book.docs[i].first_publish_year + book.docs[i].title + book.docs[i].has_fulltext;
+    bookList.appendChild(row);
+    row.appendChild(cell);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    row.appendChild(cell4);
+    row.appendChild(cell5);
+  }
 }
